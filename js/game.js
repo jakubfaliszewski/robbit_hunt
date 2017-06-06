8a33a62d48f96741390cedeaa5373cde3e50e2a3
jakubfaliszewski <jfaliszewski@interia.pl>
2017-06-06 22:38:35 +0200

    1st upload

---
+++ js/game.js
@@ -0,0 +1,124 @@
+$(document).ready(function()
+{
+    $("button").click(function()
+    {
+        $(this).hide();
+        start = setInterval(GameIsOn, 600);
+    });
+
+});
+
+var r1none = document.getElementById("robbit1").style.pointerEvents = "none";
+var r2none = document.getElementById("robbit2").style.pointerEvents = "none";
+var r3none = document.getElementById("robbit3").style.pointerEvents = "none";
+var r4none = document.getElementById("robbit4").style.pointerEvents = "none";
+var robbit1=document.getElementById('robbit1');
+var robbit2=document.getElementById('robbit2');
+var robbit3=document.getElementById('robbit3');
+var robbit4=document.getElementById('robbit4');
+var content=document.getElementById('content');
+var point=document.getElementById('pointvalue');
+pointvalue=point.value;
+pointvalue = 0;
+var bullet=document.getElementById('bulletvalue');
+bulletvalue=bullet.value;
+bulletvalue = 20;
+
+function GameIsOn()
+{
+    time=300;
+    random=Math.random();
+    if(random<=0.25)
+    {
+        robbit1.style.pointerEvents = "auto";
+        $("#robbit1").animate
+        ({
+          top: '-=100px',
+          left: '+=20px'
+        }, "fast").delay(time).animate
+        ({
+          top: '+=100px',
+          left: '-=20px',
+        }, "fast");
+        setTimeout(function(){robbit1.style.pointerEvents = "none";}, 900);
+    }
+      else if(random>0.25 && random<=0.50)
+      {
+          robbit2.style.pointerEvents = "auto";
+          $("#robbit2").animate
+          ({
+            top: '-=100px',
+            left: '-=20px'
+          }, "fast").delay(time).animate
+          ({
+            top: '+=100px',
+            left: '+=20px'
+          }, "fast");
+          setTimeout(function(){robbit2.style.pointerEvents = "none";}, 900);
+      }
+        else if(random>0.50 && random<=0.75)
+        {
+            robbit3.style.pointerEvents = "auto";
+            $("#robbit3").animate
+            ({
+              top: '-=100px',
+              left: '+=40px'
+            }, "fast").delay(time).animate
+            ({
+              top: '+=100px',
+              left: '-=40px'
+            }, "fast");
+            setTimeout(function(){robbit3.style.pointerEvents = "none";}, 900);
+        }
+          else if(random>0.75)
+          {
+              robbit4.style.pointerEvents = "auto";
+              $("#robbit4").animate
+              ({
+                top: '-=110px',
+                left: '-=40px'
+              }, "fast").delay(time).animate
+              ({
+                top: '+=110px',
+                left: '+=40px'
+              }, "fast");
+              setTimeout(function(){robbit4.style.pointerEvents = "none";}, 900);
+            }
+}
+
+$("button").click(function pointsystem()
+{
+  robbit1.onmousedown=pointup;
+  robbit2.onmousedown=pointup;
+  robbit3.onmousedown=pointup;
+  robbit4.onmousedown=pointup;
+  function pointup()
+  {
+    pointvalue++;
+    document.getElementById("pointvalue").innerHTML = pointvalue;
+    if(pointvalue>=20)
+    {
+      clearInterval(start);
+      sum();
+    }
+  }
+
+  content.onmouseup=bulletdown;
+  function bulletdown()
+  {
+    bulletvalue--;
+    document.getElementById("bulletvalue").innerHTML = bulletvalue;
+    if(bulletvalue<=0)
+    {
+      clearInterval(start);
+      sum();
+    }
+  }
+});
+
+function sum()
+{
+
+  document.getElementById("end").style.display="block";
+  document.getElementById("sumup").innerHTML = (pointvalue+" points and "+bulletvalue+" bullets left<br/> accuracy: "+Math.round(pointvalue/20*100)+"%");
+}
