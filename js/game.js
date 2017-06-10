5b99009b6578253756fc7199edf469cd155b810b
jakubfaliszewski <jfaliszewski@interia.pl>
2017-06-10 16:18:37 +0200

    kolejna poprawa semantyki

--- js/game.js
+++ js/game.js
@@ -1,29 +1,21 @@
-$(document).ready(function()
-{
-    $("button").click(function()
-    {
-        $(this).hide();
-        start = setInterval(GameIsOn, 800);
-    });
-
-});
-
+// definiowanie
 var r1none = document.getElementById("robbit1").style.pointerEvents = "none";
 var r2none = document.getElementById("robbit2").style.pointerEvents = "none";
 var r3none = document.getElementById("robbit3").style.pointerEvents = "none";
 var r4none = document.getElementById("robbit4").style.pointerEvents = "none";
-var robbit1=document.getElementById('robbit1');
-var robbit2=document.getElementById('robbit2');
-var robbit3=document.getElementById('robbit3');
-var robbit4=document.getElementById('robbit4');
-var content=document.getElementById('content');
-var point=document.getElementById('pointvalue');
+var robbit1=document.getElementById("robbit1");
+var robbit2=document.getElementById("robbit2");
+var robbit3=document.getElementById("robbit3");
+var robbit4=document.getElementById("robbit4");
+var content=document.getElementById("content");
+var point=document.getElementById("pointvalue");
 var pointvalue=point.value;
 pointvalue = 0;
-var bullet=document.getElementById('bulletvalue');
+var bullet=document.getElementById("bulletvalue");
 var bulletvalue=bullet.value;
 bulletvalue = 20;
 
+// losowanie robbitow
 function GameIsOn()
 {
     var time=200;
@@ -47,12 +39,12 @@ function GameIsOn()
           robbit2.style.pointerEvents = "auto";
           $("#robbit2").animate
           ({
-            top: '-=100px',
-            left: '-=20px'
+            top: "-=100px",
+            left: "-=20px"
           }, "fast").delay(time).animate
           ({
-            top: '+=100px',
-            left: '+=20px'
+            top: "+=100px",
+            left: "'+=20px"
           }, "fast");
           setTimeout(function(){robbit2.style.pointerEvents = "none";}, 800);
       }
@@ -61,12 +53,12 @@ function GameIsOn()
             robbit3.style.pointerEvents = "auto";
             $("#robbit3").animate
             ({
-              top: '-=100px',
-              left: '+=40px'
+              top: "-=100px",
+              left: "+=40px"
             }, "fast").delay(time).animate
             ({
-              top: '+=100px',
-              left: '-=40px'
+              top: "+=100px",
+              left: "-=40px"
             }, "fast");
             setTimeout(function(){robbit3.style.pointerEvents = "none";}, 800);
         }
@@ -75,23 +67,30 @@ function GameIsOn()
               robbit4.style.pointerEvents = "auto";
               $("#robbit4").animate
               ({
-                top: '-=110px',
-                left: '-=40px'
+                top: "-=110px",
+                left: "-=40px"
               }, "fast").delay(time).animate
               ({
-                top: '+=110px',
-                left: '+=40px'
+                top: "+=110px",
+                left: "+=40px"
               }, "fast");
               setTimeout(function(){robbit4.style.pointerEvents = "none";}, 800);
             }
 }
 
+// podsumowanie
+
+function sum()
+{
+
+  document.getElementById("end").style.display="block";
+  document.getElementById("sumup").innerHTML = (pointvalue+" points and "+bulletvalue+" bullets left<br/> accuracy: "+Math.round(pointvalue/20*100)+"%");
+}
+
+// system punktowania
+
 $("button").click(function pointsystem()
 {
-  robbit1.onmousedown=pointup;
-  robbit2.onmousedown=pointup;
-  robbit3.onmousedown=pointup;
-  robbit4.onmousedown=pointup;
   function pointup()
   {
     pointvalue++;
@@ -102,6 +101,11 @@ $("button").click(function pointsystem()
       sum();
     }
   }
+
+  robbit1.onmousedown=pointup;
+  robbit2.onmousedown=pointup;
+  robbit3.onmousedown=pointup;
+  robbit4.onmousedown=pointup;
 
   content.onmouseup=bulletdown;
   function bulletdown()
@@ -119,9 +123,15 @@ $("button").click(function pointsystem()
   }
 });
 
-function sum()
-{
 
-  document.getElementById("end").style.display="block";
-  document.getElementById("sumup").innerHTML = (pointvalue+" points and "+bulletvalue+" bullets left<br/> accuracy: "+Math.round(pointvalue/20*100)+"%");
-}
+// new game
+
+$(document).ready(function()
+{
+    $("button").click(function()
+    {
+        $(this).hide();
+        start = setInterval(GameIsOn, 800);
+    });
+
+});
